908315c82860f1e41a15fde9f48f6d6a95ce0028
Tristan Libeau <tristan.libeau@gmail.com>
2021-10-25 12:57:09 +1100

    spliting in 2 sides

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,8 +9,33 @@ const Header = () => {
         <FaFileAlt />
       </div>
       <h1>CV Builder <span style={{fontSize: 20, fontStyle: "italic"}}></span></h1>
+      {/* <Tab.Container defaultActiveKey='login'>
+          <Modal.Header closeButton>
+            <Modal.Title id='signup-modal'>
+              <Nav variant='pills'>
+                <Nav.Item>
+                  <Nav.Link eventKey='login'>Login</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey='signup'>Sign Up</Nav.Link>
+                </Nav.Item>
+              </Nav>
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Tab.Content>
+              <Tab.Pane eventKey='login'>
+                <LoginForm handleModalClose={() => setShowModal(false)} />
+              </Tab.Pane>
+              <Tab.Pane eventKey='signup'>
+                <SignUpForm handleModalClose={() => setShowModal(false)} />
+              </Tab.Pane>
+            </Tab.Content>
+          </Modal.Body>
+        </Tab.Container> */}
     </div>
+    
   );
 };
 
-export default Header;
+export default Header
