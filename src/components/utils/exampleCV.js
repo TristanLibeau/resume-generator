e4c13503dbca7e0703e35068e33e84b8b3f92e94
Tristan Libeau <tristan.libeau@gmail.com>
2021-10-25 21:06:14 +1100

    adding aboutme section to example

--- src/components/utils/exampleCV.js
+++ src/components/utils/exampleCV.js
@@ -12,6 +12,9 @@ const exampleCV = {
     phone: "+61333333333",
     email: "fakeemail@gmail.com",
   },
+  about: {
+    textValue:"There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary.",
+  },
   experience: [
     {
       id: uuidv4(),
