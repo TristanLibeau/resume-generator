5a75e95f73cd45f9b5925b4feb8a720b3f5b4bb0
Tristan Libeau <tristan.libeau@gmail.com>
2021-10-28 19:59:46 +1100

    quick fix

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -26,13 +26,14 @@ const SignupForm = () => {
 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
+    console.log(userFormData)
 
     // check if form has everything (as per react-bootstrap docs)
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
+    // const form = event.currentTarget;
+    // if (form.checkValidity() === false) {
+    //   event.preventDefault();
+    //   event.stopPropagation();
+    // }
 
     try {
       const { data } = await addUser({
